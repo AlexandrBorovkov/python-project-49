f133ddcd3e4d99f8338431dd9c07a9e37d9321a5
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2024-10-08 22:46:25 +0500

    correction lint

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3.12
 
 
-from brain_games.games.service_brain_progression import start_game, answer_processing
+from brain_games.games.service_brain_progression import \
+    start_game, answer_processing
 
 
 def main():
