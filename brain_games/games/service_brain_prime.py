f4d646a2988b63640431dbe678289583efa4948e
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2024-10-10 10:37:05 +0500

    refactoring brain-prime

--- brain_games/games/service_brain_prime.py
+++ brain_games/games/service_brain_prime.py
@@ -1,42 +1,25 @@
-import prompt
 import random
+import prompt
+from brain_games.games.core import greeting, cycle
 
 
-def start_game():
-    print("Welcome to the Brain Games!")
-    username = prompt.string("May I have your name? ")
-    print(f"Hello, {username}!")
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    return username
+def create_question():
+    number = random.randint(1, 999)
+    return number
 
 
-def output_wrong_answer(user_response, right_answer, username):
-    print(
-        f"'{user_response}' is wrong answer ;(.",
-        f"Correct answer was '{right_answer}'.",
-        f"\nLet's try again, {username}!",
-        sep=" "
-    )
+def get_user_response():
+    return prompt.string("Your answer: ")
 
 
-def check_number(num):
-    result = len([x for x in range(1, num // 2 + 1) if num % x == 0])
+def get_right_answer(question):
+    result = len([x for x in range(1, question // 2 + 1) if question % x == 0])
     if result == 1:
         return "yes"
     return "no"
 
 
-def answer_processing(username):
-    counter = 3
-    while counter > 0:
-        number = random.randint(1, 99)
-        print(f"Question: {number}")
-        user_response = prompt.string("Your answer: ")
-        right_answer = check_number(number)
-        if user_response == right_answer:
-            print("Correct!")
-            counter -= 1
-        else:
-            output_wrong_answer(user_response, right_answer, username)
-            return
-    print(f"Congratulations, {username}!")
+def start_game():
+    INDEX_LIST_QUESTIONS = 3
+    username = greeting(INDEX_LIST_QUESTIONS)
+    cycle(username, create_question, get_user_response, get_right_answer)
