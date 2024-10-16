916048561c4505585f977fa0a3a59955646d643e
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2024-10-16 12:12:40 +0500

    refactoring cli.welcome_user

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,6 @@ import prompt
 
 
 def welcome_user():
-    print("brain-games", "Welcome to the Brain Games!", sep="\n")
-    name = prompt.string("May I have your name? ")
+    name = prompt.string("Welcome to the Brain Games!\n"
+                         "May I have your name? ")
     print(f"Hello, {name}!")
