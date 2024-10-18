b3070da2064c5abe3ec5883da9dfb473f3bd4c28
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2024-10-18 11:31:04 +0500

    refactoring prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,10 +1,6 @@
-from brain_games.core import greeting, cycle, get_user_response_string
+from brain_games.constants import PRIME_INSTRUCTION
+from brain_games.core import cycle
 from brain_games.utils import get_random_number
-
-
-def create_question():
-    number = get_random_number(1, 99)
-    return number
 
 
 def get_right_answer(question):
@@ -15,11 +11,13 @@ def get_right_answer(question):
     return "no"
 
 
+def create_question():
+    question = get_random_number(1, 99)
+    right_answer = get_right_answer(question)
+    return question, right_answer
+
+
 def start_game():
-    INDEX_LIST_QUESTIONS = 3
-    username = greeting(INDEX_LIST_QUESTIONS)
-    cycle(username,
-          create_question,
-          get_user_response_string,
-          get_right_answer
+    cycle(create_question,
+          PRIME_INSTRUCTION
           )
