523150f645a64b9e5a31c57695b4dc682b631e57
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2024-10-18 10:54:26 +0500

    refactoring gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,24 +1,18 @@
 import math
-from brain_games.core import greeting, cycle, get_user_response_integer
+from brain_games.constants import GCD_INSTRUCTION
+from brain_games.core import cycle
 from brain_games.utils import get_random_number
 
 
 def create_question():
     number_1 = get_random_number(1, 99)
     number_2 = get_random_number(1, 99)
-    return f"{number_1} {number_2}"
-
-
-def get_right_answer(question):
-    number_1, number_2 = [int(x) for x in question.split()]
-    return math.gcd(number_1, number_2)
+    question = f"{number_1} {number_2}"
+    right_answer = math.gcd(number_1, number_2)
+    return question, right_answer
 
 
 def start_game():
-    INDEX_LIST_QUESTIONS = 2
-    username = greeting(INDEX_LIST_QUESTIONS)
-    cycle(username,
-          create_question,
-          get_user_response_integer,
-          get_right_answer
+    cycle(create_question,
+          GCD_INSTRUCTION
           )
