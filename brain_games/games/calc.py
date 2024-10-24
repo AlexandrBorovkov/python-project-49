db9d7e392fd02275b0360835e62473e724b61aeb
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2024-10-24 14:33:19 +0500

    create choice_math_operations

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,12 +4,20 @@ from brain_games.core import run_game
 from brain_games.utils import get_random_number
 
 
+def choice_math_operations(number_1, number_2):
+    math_operations = [
+        ("+", number_1 + number_2),
+        ("-", number_1 - number_2),
+        ("*", number_1 * number_2)
+    ]
+    return choice(math_operations)
+
+
 def create_question():
-    number_1 = get_random_number(1, 99)
-    number_2 = get_random_number(1, 99)
-    char = choice(["+", "-", "*"])
-    question = f"{number_1} {char} {number_2}"
-    right_answer = str(eval(question))
+    number_1, number_2 = get_random_number(1, 99), get_random_number(1, 99)
+    math_operation = choice_math_operations(number_1, number_2)
+    question = f"{number_1} {math_operation[0]} {number_2}"
+    right_answer = str(math_operation[1])
     return question, right_answer
 
 
