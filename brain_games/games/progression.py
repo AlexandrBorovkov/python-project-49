3cd01d45e92bd9bf511c0ba4d831b15c1503ed66
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2024-10-24 15:22:45 +0500

    refactoring progression.py, change functions name

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,19 +3,19 @@ from brain_games.core import run_game
 from brain_games.utils import get_random_number
 
 
-def create_question():
+def get_numerical_progression_and_answer():
     len_list = get_random_number(5, 10)
     index = get_random_number(0, len_list - 1)
     start = get_random_number(11, 99)
     step = get_random_number(2, 9)
-    question = " ".join([".." if i == index else str(step * i + start)
-                         for i in range(len_list)
-                         ])
-    right_answer = str(step * index + start)
-    return question, right_answer
+    progression = " ".join([".." if i == index else str(step * i + start)
+                            for i in range(len_list)
+                            ])
+    answer = str(step * index + start)
+    return progression, answer
 
 
 def start_game():
-    run_game(create_question,
+    run_game(get_numerical_progression_and_answer,
              PROGRESSION_INSTRUCTION
              )
