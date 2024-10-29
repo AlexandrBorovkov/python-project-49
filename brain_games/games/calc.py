165414b875bb6d43116fe26cd2baed6e8058684c
Alexandr Borovkov <mr.borovkov.prm@gmail.com>
2024-10-29 10:19:43 +0500

    finishing edits

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,20 +5,17 @@ from brain_games.utils import get_random_number
 
 
 def choice_math_operations(number_1, number_2):
-    math_operations = [
+    return choice([
         ("+", number_1 + number_2),
         ("-", number_1 - number_2),
         ("*", number_1 * number_2)
-    ]
-    return choice(math_operations)
+    ])
 
 
 def get_an_example_and_answer():
     number_1, number_2 = get_random_number(1, 99), get_random_number(1, 99)
     math_operation = choice_math_operations(number_1, number_2)
-    question = f"{number_1} {math_operation[0]} {number_2}"
-    answer = str(math_operation[1])
-    return question, answer
+    return f"{number_1} {math_operation[0]} {number_2}", str(math_operation[1])
 
 
 def start_game():
